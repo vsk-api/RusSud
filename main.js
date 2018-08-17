b5c8552f354a8b2a9debdf3c910c039848b3a080
OlegSirik <oleg.sirik@mail.ru>
2018-08-17 19:00:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,14 +14,14 @@ app.get('/send', function (req, res) {
  sendM("test", res); 
 });
 
-app.post('/callbacks', function (req, res) { 
+app.post('/callback', function (req, res) { 
 
 var body = '';
  req.on('data', function (data) {
   body += data;
  });
 
- request.on('end', function () {
+ req.on('end', function () {
    sendM( body , res);
   });
 });
