349ac42e7f05b311fda2dc25bc96a9dd3d9917d2
OlegSirik <oleg.sirik@mail.ru>
2018-08-17 09:35:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,9 +11,36 @@ app.get('/stop', function (req, res) {
  process.exit(143); 
 });
 
+app.get('/send', function (req, res) {
+ sendM("aasdas"); 
+});
+
 
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port ' + process.env.PORT + '!');
 });
  
-               
+function sendM(pBody) {
+ var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'vsp_partapi@gmail.com',
+    pass: '!Q2w3e$R'
+  }
+ });
+
+ var mailOptions = {
+  from: 'hz@gmail.com',
+  to: 'sirik@vsk.ru',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});               
+}
