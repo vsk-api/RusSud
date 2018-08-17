e6b0827ab05c9371e4261cdae28ab5762c1ab9ae
OlegSirik <oleg.sirik@mail.ru>
2018-08-17 08:55:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 var cheerio = require('cheerio');
 var needle = require('needle');
  
-var nodeMailer = require('nodemailer'),
-var bodyParser = require('body-parser');
+
 
 var express = require('express');
 var app = express();
@@ -15,56 +14,10 @@ app.get('/stop', function (req, res) {
  process.exit(143); 
 });
 
-app.get('/arbitr/:id', function (req, res) {
-  getKadArbitr( req.params.id, res );
-  //res.send('Hello World!');
-});
-
 
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port ' + process.env.PORT + '!');
 });
  
-function getKadArbitr(pUID, res) {
-   
-                var vData =[];
-                var myURL = 'http://kad.arbitr.ru/Card/' + pUID;
- 
-                var options = {
-                               headers:{
-                               'Host': 'kad.arbitr.ru',
-                               'Connection': 'keep-alive',
-                               'Accept': '*/*',
-                               'Origin': 'http://kad.arbitr.ru',
-                               'x-date-format': 'iso',
-                               'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
-                               'Content-Type': 'text/*; charset=utf-8',
-                               'Referer': 'http://kad.arbitr.ru/',
-                               'Accept-Encoding': 'gzip, deflate',
-                               'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
-                               }
-                }
-               
-                var case_status = "not found";
-               
-                console.log('start');
-                needle.get( myURL, options, function (err, res2){
-                               //results.push(res2.body);
-                 //              console.log(err);
-                //               console.log(res2.body);
-               
-                try { 
-                 
-                    var $ = cheerio.load(res2.body);                                                    
-                              
-                    case_status = $('div[class=b-case-header-desc]').text().trim();
-                    res.send(case_status);
-                            
-                } catch (err) {
-                    res.send(err);
-                }  
-                               console.log(err);
- 
-                });
                
 }
