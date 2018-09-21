70eceb035a536f9a88e7c68e2f5ad14450dbcfb3
OlegSirik <oleg.sirik@mail.ru>
2018-09-21 15:54:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -37,6 +37,18 @@ var body = '';
   });
 });
 
+app.post('/callback/answerCalculatePolicy.xml', function (req, res) { 
+
+var body = '';
+ req.on('data', function (data) {
+  body += data;
+ });
+
+ req.on('end', function () {
+   sendM( 'sirik@vsk.ru', body , res);
+  });
+});
+
 app.post('/callback2', function (req, res) { 
 
 var body = '';
